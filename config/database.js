b7a2a5c765a8b9478b6b42ec323cec70e353c73f
jonyolmeda <jonyolmeda@gmail.com>
2022-11-10 00:23:49 -0300

    Estructuración de backend

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('dotenv')
+
+let connection = async () => {
+    try{
+        mongoose.connect(
+            process.env.LINK_DB,{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                
+        }
+        )
+        console.log('connected to data base');
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+connection()
