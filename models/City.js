d51cdb4b0859b662a24e5fcae2a6e94b0d52b34f
jonyolmeda <jonyolmeda@gmail.com>
2022-11-10 21:33:28 -0300

    Definir el modelo City

--- models/City.js
+++ models/City.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     continent: {type: String, required: true},
     photo:{type: String, required: true},
     population: {type: Number, required: true},
-    userId:{type: String, ref:"users" ,required: true}
+    userId:{type: mongoose.Types.ObjectId, ref:"users" ,required: true}
 })
 const City = mongoose.model('cities',schema);
 module.exports = City
