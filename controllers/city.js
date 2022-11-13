b177d1bb1a451c4348d40dbaafc581e4d4d2289b
jonyolmeda <jonyolmeda@gmail.com>
2022-11-12 21:22:52 -0300

    Creación de filtros

--- controllers/city.js
+++ controllers/city.js
@@ -17,6 +17,35 @@ create: async(req, res) => {
         })
     }
     },
+
+    read: async(req, res) => {
+        let { query } = req
+        console.log(req.query)
+        if (req.query.name) {
+            query = req.query.name
+        }
+        if (req.query.continent) {
+            query = {
+                ...query,
+                continent: req.query.continent
+            }
+        }
+        try {
+            let read_city = await City.find(query)
+            res.status(200).json({
+                response: read_city,
+                success: true,
+                message: 'The cities has been found'
+            })
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+        },
 }
+
+
 
 module.exports = controller
