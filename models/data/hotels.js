9e14a00705c1ce972fb42e26ba592b71f71a362d
pungitoregerman <pungitoregerman@gmail.com>
2022-11-13 00:42:23 -0300

    creacion controlador y ruta de hotels

--- models/data/hotels.js
+++ models/data/hotels.js
@@ -1,112 +1,148 @@
 let hotelsCasino = [
-	{	
-	id: 'hc1',
-        name: 'Plaza Inn Doha',
-        photo: ['https://ak-d.tripcdn.com/images/200h13000000vhoy51385_Z_1100_824_R5_Q70_D.jpg','https://www.kayak.ae/rimg/himg/06/f5/f4/expediav2-300009-4bd2b2-931479.jpg?width=1366&height=768&crop=true','https://foto.hrsstatic.com/fotos/0/3/545/350/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F1%2F1%2F8%2F411817%2F411817_z_11632759.jpg/bVCYRVE%2FRIdSkD%2FWmQwCJg%3D%3D/506,336/6/Plaza_Inn_Doha-Doha-Standardzimmer-7-411817.jpg'],
-        capacity: 5000 ,
-        cityId: '636d9804d8061af971997443',
-        userId: '636d51715d29e99d62636bd9'
-	},
-{	
-	id: 'hc2',
-        name: 'Four Seasons Hotel',
-        photo: ['https://static.toiimg.com/photo/53868289.cms','https://ik.imgkit.net/3vlqs5axxjf/external/http://www.cfmedia.vfmleonardo.com/imageRepo/4/0/64/216/679/Four_Seasons_Hotel_Sydney_lobby_O.jpg','https://media.cntraveler.com/photos/5bbe160a8955df3d45552ae7/16:9/w_2560%2Cc_limit/Four-Seasons-Hotel-Sydney__2018_Royal-Suite%2C-Four-Seasons-Hotel-Sydney.jpg'],
-        capacity:  3500,
-        cityId: '636d9804d8061af971997447',
-        userId: '636d51715d29e99d62636bd9'
-	},
-{	
-	id: 'hc3',
-        name: 'Macao Cancun',
-        photo: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ6CbSKGMu03Oh69S_hiOny6MTXdZ17EbH_ViFeXCJpLH0aZrh4bqKsF-9Vjot0BzoXNE&usqp=CAU','https://quintafuerzamx.s3.us-east-2.amazonaws.com/Leopard/uploads/2020/08/casino-macao-696x464.jpg','https://www.macaocancun.com/wp-content/uploads/2021/07/Ruleta.jpg'],
-        capacity: 4000 ,
-        cityId: '636d9804d8061af971997446',
-        userId: '636d51715d29e99d62636bd9'
-	},
-{	
-	id: 'hc4',
-        name: 'Mod Hotels Mendoza',
-        photo: ['https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/69/81/2b/mod-hotel-mendoza.jpg?w=700&h=-1&s=1','https://media-cdn.tripadvisor.com/media/photo-s/09/69/81/d2/mod-hotel-mendoza.jpg','https://x.cdrst.com/foto/hotel-sf/14f45/medianaresp/mod-hotels-mendoza-general-ca30aa5.jpg'],
-        capacity:  3000,
-        cityId: '636d9804d8061af971997442',
-        userId: '636d51715d29e99d62636bda'
-	},
-{	
-	id: 'hc5',
-        name: 'Graton Resort & Casino',
-        photo: ['https://imengine.prod.srp.navigacloud.com/?uuid=EA8FDF84-F5B4-41EC-A9D5-4B3878CAFBB9&type=primary&q=72&width=1024','https://imengine.prod.srp.navigacloud.com/?uuid=484E6B12-1D01-4BE2-B415-38C224D37E4F&type=primary&q=72&width=800','http://www.indianz.com/IndianGaming/2018/10/03/gratonresortcasino.jpg'],
-        capacity:  3500,
-        cityId: '636d9804d8061af971997449',
-        userId: '636d51715d29e99d62636bda'
-	},
-{	
-	id: 'hc6',
-        name: 'Park Hyatt',
-        photo: ['https://cdn.kiwicollection.com/media/property/PR003646/l/003646-02-PHT%20Exterior.jpg?cb=1454011165','https://www.kayak.com.ar/rimg/himg/ba/a5/c1/ice-49913-61170639_3XL-545505.jpg?width=1366&height=768&crop=true','https://skift.com/wp-content/uploads/2018/09/Image-from-iOS-1-1024x803.jpg'],
-        capacity:  2500,
-        cityId: '636d9804d8061af971997444',
-        userId: '636d51715d29e99d62636bda'
-	},
-{	
-	id: 'hc7',
-        name: 'Hard Rock Hotel New',
-        photo: ['https://hospitality-on.com/sites/default/files/styles/image994x499/public/import/contenu/Hard_Rock_hotel_casino_new_york.jpg?h=1a72c976&itok=OEedYPNo','https://conocedores.com/wp-content/uploads/2022/05/hard-rock-nueva-york-viajes-destinos-estados-unidos-12050222.webp','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5oQ8ftvCo44LfqfIfhb6d0QoAfVumXLHgyf9K-3bWVZEwywI66r-0GH42cQN9cJ6bn9o&usqp=CAU'],
-        capacity: 3250 ,
-        cityId: '636d9804d8061af971997440',
-        userId: '636d51715d29e99d62636bd8'
-	},
-{	
-	id: 'hc8',
-        name:'CitizenM Tower of London' ,
-        photo: ['https://assets.citizenm.com/images/CitizenM_Tower_2_UK-0065_1500px_10905dc4d824c17d5dd3f7de-2.jpg','https://img.traveltriangle.com/cms/attachments/pictures/1018977/original/citizenmltl_3_2_008544b04b02a3.jpg?tr=w-606,h-400','https://assets.citizenm.com/images/hotel-bedroom-plush-bedding-wall-to-wall-window-view-Tower-of-London-2_f1079451e4b0294621cb07c-1.jpg'],
-        capacity:  2200,
-        cityId: '636d9804d8061af97199743f',
-        userId: '636d51715d29e99d62636bd8'
-	},
-{	
-	id: 'hc9',
-        name: 'Marina Bay',
-        photo: ['https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/3c/3a/e6/exterior.jpg?w=700&h=-1&s=1','https://i.pinimg.com/originals/00/2f/95/002f95068bbcb53a295303bf68d58394.jpg','https://www.kayak.com.ar/rimg/kimg/94/64/080350a1279d8ae7.jpg?width=1366&height=768&crop=true'],
-        capacity: 6500 ,
-        cityId: '636d9804d8061af97199744a',
-        userId: '636d51715d29e99d62636bd8'
-	},
-{	
-	id: 'hc10',
-        name: 'Le Bristol Paris',
-        photo: ['https://img.ev.mu/images/hotels/43736/600x400/615650.jpg','https://entreprisedigitale.info/wp-content/uploads/2016/05/Le_Bristol_Paris_-_Jardin_-_C_Marianne_Majerus.png','https://media.viajando.travel/adjuntos/271/imagenes/000/282/0000282855.jpg?0000-00-00-00-00-00'],
-        capacity:  3200,
-        cityId: '636d9804d8061af971997441',
-        userId: '636d51715d29e99d62636bd7'
-	},
-{	
-	id: 'hc11',
-        name: 'Fairmont Nile City',
-        photo: ['https://www.lapatilla.com/wp-content/uploads/2020/08/Fairmont-Nile-City-Hotel.jpg','https://www.kayak.com.ar/rimg/himg/61/81/fa/ice-229900374-70356552_3XL-059180.jpg?width=1366&height=768&crop=true','https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/1e/ea/e5/deluxe-nile-room.jpg?w=700&h=-1&s=1'],
-        capacity: 3800 ,
-        cityId: '636d9804d8061af971997446',
-        userId: '636d51715d29e99d62636bd7'
-	},
-{	
-	id: 'hc12',
-        name: 'Athina Luxury Suites',
-        photo: ['https://media.staticontent.com/media/pictures/22ff6d16-0ada-4b82-a388-511a4369b05f','https://www.athinasuites.com/images/Homepage_Resort_INDEX/deluxe-suite.jpg','https://santorinidave.com/wp-content/uploads/2021/05/athina-luxury-suites-hotel-fira-santorini.jpeg'],
-        capacity: 1800 ,
-        cityId: '636d9804d8061af971997448',
-        userId: '636d51715d29e99d62636bd7'
-	},
-]
+  {
+    name: "Plaza Inn Doha",
+    photo: [
+      "https://ak-d.tripcdn.com/images/200h13000000vhoy51385_Z_1100_824_R5_Q70_D.jpg",
+      "https://www.kayak.ae/rimg/himg/06/f5/f4/expediav2-300009-4bd2b2-931479.jpg?width=1366&height=768&crop=true",
+      "https://foto.hrsstatic.com/fotos/0/3/545/350/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F1%2F1%2F8%2F411817%2F411817_z_11632759.jpg/bVCYRVE%2FRIdSkD%2FWmQwCJg%3D%3D/506,336/6/Plaza_Inn_Doha-Doha-Standardzimmer-7-411817.jpg",
+    ],
+    capacity: 5000,
+    cityId: "636d9804d8061af971997443",
+    userId: "636d51715d29e99d62636bd9",
+  },
+  {
+    name: "Four Seasons Hotel",
+    photo: [
+      "https://static.toiimg.com/photo/53868289.cms",
+      "https://ik.imgkit.net/3vlqs5axxjf/external/http://www.cfmedia.vfmleonardo.com/imageRepo/4/0/64/216/679/Four_Seasons_Hotel_Sydney_lobby_O.jpg",
+      "https://media.cntraveler.com/photos/5bbe160a8955df3d45552ae7/16:9/w_2560%2Cc_limit/Four-Seasons-Hotel-Sydney__2018_Royal-Suite%2C-Four-Seasons-Hotel-Sydney.jpg",
+    ],
+    capacity: 3500,
+    cityId: "636d9804d8061af971997447",
+    userId: "636d51715d29e99d62636bd9",
+  },
+  {
+    name: "Macao Cancun",
+    photo: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ6CbSKGMu03Oh69S_hiOny6MTXdZ17EbH_ViFeXCJpLH0aZrh4bqKsF-9Vjot0BzoXNE&usqp=CAU",
+      "https://quintafuerzamx.s3.us-east-2.amazonaws.com/Leopard/uploads/2020/08/casino-macao-696x464.jpg",
+      "https://www.macaocancun.com/wp-content/uploads/2021/07/Ruleta.jpg",
+    ],
+    capacity: 4000,
+    cityId: "636d9804d8061af971997446",
+    userId: "636d51715d29e99d62636bd9",
+  },
+  {
+    name: "Mod Hotels Mendoza",
+    photo: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/69/81/2b/mod-hotel-mendoza.jpg?w=700&h=-1&s=1",
+      "https://media-cdn.tripadvisor.com/media/photo-s/09/69/81/d2/mod-hotel-mendoza.jpg",
+      "https://x.cdrst.com/foto/hotel-sf/14f45/medianaresp/mod-hotels-mendoza-general-ca30aa5.jpg",
+    ],
+    capacity: 3000,
+    cityId: "636d9804d8061af971997442",
+    userId: "636d51715d29e99d62636bda",
+  },
+  {
+    name: "Graton Resort & Casino",
+    photo: [
+      "https://imengine.prod.srp.navigacloud.com/?uuid=EA8FDF84-F5B4-41EC-A9D5-4B3878CAFBB9&type=primary&q=72&width=1024",
+      "https://imengine.prod.srp.navigacloud.com/?uuid=484E6B12-1D01-4BE2-B415-38C224D37E4F&type=primary&q=72&width=800",
+      "http://www.indianz.com/IndianGaming/2018/10/03/gratonresortcasino.jpg",
+    ],
+    capacity: 3500,
+    cityId: "636d9804d8061af971997449",
+    userId: "636d51715d29e99d62636bda",
+  },
+  {
+    name: "Park Hyatt",
+    photo: [
+      "https://cdn.kiwicollection.com/media/property/PR003646/l/003646-02-PHT%20Exterior.jpg?cb=1454011165",
+      "https://www.kayak.com.ar/rimg/himg/ba/a5/c1/ice-49913-61170639_3XL-545505.jpg?width=1366&height=768&crop=true",
+      "https://skift.com/wp-content/uploads/2018/09/Image-from-iOS-1-1024x803.jpg",
+    ],
+    capacity: 2500,
+    cityId: "636d9804d8061af971997444",
+    userId: "636d51715d29e99d62636bda",
+  },
+  {
+    name: "Hard Rock Hotel New",
+    photo: [
+      "https://hospitality-on.com/sites/default/files/styles/image994x499/public/import/contenu/Hard_Rock_hotel_casino_new_york.jpg?h=1a72c976&itok=OEedYPNo",
+      "https://conocedores.com/wp-content/uploads/2022/05/hard-rock-nueva-york-viajes-destinos-estados-unidos-12050222.webp",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5oQ8ftvCo44LfqfIfhb6d0QoAfVumXLHgyf9K-3bWVZEwywI66r-0GH42cQN9cJ6bn9o&usqp=CAU",
+    ],
+    capacity: 3250,
+    cityId: "636d9804d8061af971997440",
+    userId: "636d51715d29e99d62636bd8",
+  },
+  {
+    name: "CitizenM Tower of London",
+    photo: [
+      "https://assets.citizenm.com/images/CitizenM_Tower_2_UK-0065_1500px_10905dc4d824c17d5dd3f7de-2.jpg",
+      "https://img.traveltriangle.com/cms/attachments/pictures/1018977/original/citizenmltl_3_2_008544b04b02a3.jpg?tr=w-606,h-400",
+      "https://assets.citizenm.com/images/hotel-bedroom-plush-bedding-wall-to-wall-window-view-Tower-of-London-2_f1079451e4b0294621cb07c-1.jpg",
+    ],
+    capacity: 2200,
+    cityId: "636d9804d8061af97199743f",
+    userId: "636d51715d29e99d62636bd8",
+  },
+  {
+    name: "Marina Bay",
+    photo: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/3c/3a/e6/exterior.jpg?w=700&h=-1&s=1",
+      "https://i.pinimg.com/originals/00/2f/95/002f95068bbcb53a295303bf68d58394.jpg",
+      "https://www.kayak.com.ar/rimg/kimg/94/64/080350a1279d8ae7.jpg?width=1366&height=768&crop=true",
+    ],
+    capacity: 6500,
+    cityId: "636d9804d8061af97199744a",
+    userId: "636d51715d29e99d62636bd8",
+  },
+  {
+    name: "Le Bristol Paris",
+    photo: [
+      "https://img.ev.mu/images/hotels/43736/600x400/615650.jpg",
+      "https://entreprisedigitale.info/wp-content/uploads/2016/05/Le_Bristol_Paris_-_Jardin_-_C_Marianne_Majerus.png",
+      "https://media.viajando.travel/adjuntos/271/imagenes/000/282/0000282855.jpg?0000-00-00-00-00-00",
+    ],
+    capacity: 3200,
+    cityId: "636d9804d8061af971997441",
+    userId: "636d51715d29e99d62636bd7",
+  },
+  {
+    name: "Fairmont Nile City",
+    photo: [
+      "https://www.lapatilla.com/wp-content/uploads/2020/08/Fairmont-Nile-City-Hotel.jpg",
+      "https://www.kayak.com.ar/rimg/himg/61/81/fa/ice-229900374-70356552_3XL-059180.jpg?width=1366&height=768&crop=true",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/1e/ea/e5/deluxe-nile-room.jpg?w=700&h=-1&s=1",
+    ],
+    capacity: 3800,
+    cityId: "636d9804d8061af971997446",
+    userId: "636d51715d29e99d62636bd7",
+  },
+  {
+    name: "Athina Luxury Suites",
+    photo: [
+      "https://media.staticontent.com/media/pictures/22ff6d16-0ada-4b82-a388-511a4369b05f",
+      "https://www.athinasuites.com/images/Homepage_Resort_INDEX/deluxe-suite.jpg",
+      "https://santorinidave.com/wp-content/uploads/2021/05/athina-luxury-suites-hotel-fira-santorini.jpeg",
+    ],
+    capacity: 1800,
+    cityId: "636d9804d8061af971997448",
+    userId: "636d51715d29e99d62636bd7",
+  },
+];
 
-require ('dotenv').config()
-require('../../config/database')
-const Hotel = require('../Hotel')
+require("dotenv").config();
+require("../../config/database");
+const Hotel = require("../Hotel");
 
-hotelsCasino.forEach(elemento =>{
-    Hotel.create({
-    name:elemento.name,
-    photo:elemento.photo,
+hotelsCasino.forEach((elemento) => {
+  Hotel.create({
+    name: elemento.name,
+    photo: elemento.photo,
     capacity: elemento.capacity,
     cityId: elemento.cityId,
     userId: elemento.userId,
-})
-})
+  });
+});
