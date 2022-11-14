037b086946ced819173d925d80f20e504d82ac8d
pungitoregerman <pungitoregerman@gmail.com>
2022-11-13 23:15:17 -0300

    creacion controlador patch shows

--- controllers/show.js
+++ controllers/show.js
@@ -1,10 +1,10 @@
-const Itinerary = require('../models/Show')
+const Show = require('../models/Show')
 
  const controller = {
 create: async(req, res) => {
     
     try {
-        let new_show = await Itinerary.create(req.body)
+        let new_show = await Show.create(req.body)
         res.status(201).json({
             id: new_show._id,
             success: true,
@@ -17,5 +17,28 @@ create: async(req, res) => {
         })
     }
     },
+    update: async(req, res) => {
+        let { id } = req.params
+        try {
+            let oneShow = await Show.findOneAndUpdate({_id: id}, req.body, {new:true})
+            if (oneShow){
+                res.status(200).json({
+                id: oneShow._id,
+                success: true,
+                message: 'The show has been modificated with success'
+            })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "The show hasn't been found"
+                })
+            }       
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+        }
 }
 module.exports = controller
