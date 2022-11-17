639c181a41739687234af2948616df966ab17f7f
pungitoregerman <pungitoregerman@gmail.com>
2022-11-16 21:17:19 -0300

    detailsHotels

---
+++ routes/hotelId.js
@@ -0,0 +1,6 @@
+let  router = require('express').Router()
+let { read } = require('../controllers/hotelId')
+
+router.get('/:id',read)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ let cities = require('./city')
 let hotels = require('./hotel')
 let itineraries = require('./itinerary')
 let show = require('./show')
+let hotelId = require('./hotelId')
 
 router.use('/user', user)
 router.use('/cities', cities)
 router.use('/hotels', hotels)
+router.use('/hotel', hotelId)
 router.use('/itineraries', itineraries)
 router.use('/shows', show)
 
