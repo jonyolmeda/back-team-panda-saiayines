5dab6d6c40bed475598a2b6627dc8c4a5d9b9e68
pungitoregerman <pungitoregerman@gmail.com>
2022-11-22 01:01:01 -0300

    crear test 201, capacity, y validador schema

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,7 +1,9 @@
 let  router = require('express').Router()
 let {create,read,update,destroy} = require('../controllers/hotel')
+const schema = require('../schemas/hotel')
+const validator = require('../middlewares/validator')
 
-router.post('/',create)
+router.post('/',validator(schema),create)
 router.get('/',read)
 router.patch('/:id', update)
 router.delete('/:id', destroy)
