f2938ac3e35435423f95d5f4c71675d32d08c549
jonyolmeda <jonyolmeda@gmail.com>
2022-11-27 16:28:11 -0300

    Creación de controlador y ruta de sign out

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 const bcryptjs = require('bcryptjs')
 const crypto = require('crypto')
 const accountVerificationEmail = require('../middlewares/accountVerificationEmail')
-const { userSignedUpResponse, userNotFoundResponse,invalidCredentialsResponse} = require('../config/responses')
+const { userSignedUpResponse, userNotFoundResponse,invalidCredentialsResponse, userSignedOutResponse} = require('../config/responses')
 const jwt = require('jsonwebtoken')
 
  const controller = {
@@ -83,6 +83,15 @@ entry: async (req, res, next) => {
           next(error);
         }
       },
-}
+      unlogin: async (req, res, next) => {
+        try {
+          const { id } = req.user;
+          await User.findOneAndUpdate({ _id: id }, { logged: false });
+          return userSignedOutResponse(req, res);
+        } catch (error) {
+          next(error);
+        }
+      },
+    }
 
 module.exports = controller
