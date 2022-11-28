d1b60cd2f26b46570f24eb4fb25456292346b46b
pungitoregerman <pungitoregerman@gmail.com>
2022-11-28 15:52:50 -0300

    fixed validacion

--- config/passport.js
+++ config/passport.js
@@ -7,18 +7,17 @@ const User = require("../models/User");
 passport.use(
   new passportJwt.Strategy( 
     {
-      jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(), // de tipo bearer
-      secretOrKey: KEY_JWT, //con la clave secreta
-    }, //la estrategia devuelve la extraccion en un objeto: jwt_payload
+      jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(), 
+      secretOrKey: KEY_JWT, 
+    },
     async (jwt_payload, done) => {
       console.log(jwt_payload);
       try {
-        let user = await User.findOne({ _id: jwt_payload.id }); //buscamos el usuario
+        let user = await User.findOne({ _id: jwt_payload.id }); 
         if (user) {
           user = {
-            //este es el objeto user que se "inyecta" al req
-            //aqui es donde protejo los datos del usuario
             id: user._id,
+            userId:user._id,
             name: user.name,
             email: user.email,
             photo: user.photo,
--- routes/show.js
+++ routes/show.js
@@ -1,12 +1,14 @@
 let  router = require('express').Router()
 let schema = require ('../schemas/shows')
 const validator = require('../middlewares/validator')
+const passport = require ('../config/passport')
+const model = require('../models/Show')
 let {create, read, update, destroy} = require('../controllers/show')
 
 router.post('/',validator(schema), create)
 router.get('/', read)
-router.patch('/:id', update)
-router.delete('/:id', destroy)
+router.patch('/:id',passport.authenticate("jwt", { session: false }), update)
+router.delete('/:id',passport.authenticate("jwt", { session: false }), destroy)
 
 
 
