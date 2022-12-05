334aca3051a9a5dc14816f129dafd284bef3e4b9
pungitoregerman <pungitoregerman@gmail.com>
2022-12-05 01:29:36 -0300

    rutas y controlador update delete comment show

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,10 +5,13 @@ const controller = {
         let {user} = req;
         try{
             let new_comment = await Comments.create({
-                showId: req.body.showId,
                 userId: user.id,
+                showId: req.body.showId,
                 date: req.body.date,
-                comment: req.body.comment
+                comment: req.body.comment,
+                nameUser: user.name,
+                photoUser: user.photo,
+                roleUser: user.role
         })
         res.status(200).json({
             response: new_comment,
@@ -53,6 +56,64 @@ const controller = {
             });
         }
     },
+    update:async (req, res) => {
+      let { id } = req.params;
+      let { user } = req
+      console.log(req)
+      try {
+          let comment = await Comments.findOneAndUpdate({ _id: id }, {
+          comment: req.body.comment,
+          showId: req.body.showId,
+          date: req.body.date,
+          userId: user.id,
+          name: user.name,
+          photo: user.photo,
+      }, {
+          new: true,
+      });
+      if (comment) {
+      res.status(200).json({
+          id: comment._id,
+          success: true,
+          message: "The Comment has been modified succesfully",
+          });
+      } else {
+      res.status(404).json({
+          success: false,
+          message: "The comment was't found",
+          });
+      }
+      } catch (error) {
+          console.log(error)
+      res.status(400).json({
+          success: false,
+          message: error.message,
+      });
+  }},
+
+  destroy:async (req, res) => {
+      let { id } = req.params;
+      try {
+          let oneComment = await Comments.findOneAndDelete({ _id: id});
+          if (oneComment) {
+          res.status(200).json({
+              success: true,
+              message: "The comment has been deleted succesfully",
+          });
+      } else {
+          res.status(404).json({
+          success: false,
+          message: "The comment was't found",
+          });
+      }
+      } catch (error) {
+      res.status(400).json({
+          success: false,
+          message: error.message,
+      });
+  }
+  },
+
 
 }
 module.exports = controller;
--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+    photoUser:{type: String, required: true},
+    nameUser: {type: String, required: true},
+    roleUser: {type: String, required: true},
     showId:{type: mongoose.Types.ObjectId, ref:"shows" ,required: true},
     userId:{type: mongoose.Types.ObjectId, ref:"users" ,required: true},
     date: {type: Date, required: true},
